0733044e719e4f39d64a9f8f3847134052bed8a9
mikelros <mikelros22@gmail.com>
2016-10-13 11:44:34 +0200

    Splash now on fullscreen and would theoretically go to Main on click.

--- app/src/main/java/android/frontend/wzw/io/wzwio/SplashActivity.java
+++ app/src/main/java/android/frontend/wzw/io/wzwio/SplashActivity.java
@@ -1,13 +1,36 @@
 package android.frontend.wzw.io.wzwio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setFullscreen();
+
         setContentView(R.layout.activity_splash);
     }
+
+    private void setFullscreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // From Nexus 5.x onwards ()
+        getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onUserInteraction() {
+        super.onUserInteraction();
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
