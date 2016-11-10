491938c82cc335a0162a8a992225ccf522e0b8a3
Asgarth <asierarafat@gmail.com>
2016-11-10 11:50:53 +0100

    Form activity first

--- app/src/main/java/android/frontend/wzw/io/wzwio/FormActivity.java
+++ app/src/main/java/android/frontend/wzw/io/wzwio/FormActivity.java
@@ -22,6 +22,7 @@ public class FormActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_form);
 
